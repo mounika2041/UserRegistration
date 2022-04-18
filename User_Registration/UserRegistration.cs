665d756fe5d39d172f31eb12fd29e42ce28cb5a8
duvvamounika <mounikaduvva05@gmail.com>
2022-04-18 15:42:29 +0530

    Add LastNameUC2 branch and program

--- User_Registration/UserRegistration.cs
+++ User_Registration/UserRegistration.cs
@@ -9,19 +9,19 @@ namespace User_Registration
 {
     class UserRegistration
     {
-        public static void FirstName()
+        public static void LastName()
         {
-            string FirstName;
+            string LastName;
             var regex = "^[A-Z]{1}[a-z]{3,}$";
-            Console.WriteLine("Enter a First Name");
-            FirstName = Console.ReadLine();
-            bool result =Regex.IsMatch(FirstName, regex);
+            Console.WriteLine("Enter a Last Name");
+            LastName = Console.ReadLine();
+            bool result =Regex.IsMatch(LastName, regex);
             if (result)
             {
-                Console.WriteLine("{0} is valid firstName",FirstName);
+                Console.WriteLine("{0} is valid LastName", LastName);
             }
             else
-                Console.WriteLine("{0} is invalid firstName",FirstName);
+                Console.WriteLine("{0} is invalid LastName", LastName);
         }
     }
 }
